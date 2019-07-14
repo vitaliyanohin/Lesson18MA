fc56f93b4961b1a1d352186727a98bf0dcb08a06
Vitaliy <vitaliyanohin@gmail.com>
2019-07-14 23:16:26 +0300

    devHW25

--- src/main/java/controller/EditUserServlet.java
+++ src/main/java/controller/EditUserServlet.java
@@ -30,44 +30,24 @@ public class EditUserServlet extends HttpServlet {
   @Override
   protected void doPost(HttpServletRequest req, HttpServletResponse resp)
           throws ServletException, IOException {
-    String referer = req.getHeader("Referer");
     Long id = Long.valueOf(req.getParameter("edit"));
     String login = req.getParameter("email");
     String pass = req.getParameter("pass");
     String repeatPassword = req.getParameter("repeatPassword");
     String role = req.getParameter("role");
     User user = accountService.getUserById(id).get();
-
-    if ((login.isEmpty() & pass.isEmpty() & repeatPassword.isEmpty())
-            | (login.equals(user.getEmail()) & pass.equals(user.getPassword()))) {
-      resp.sendRedirect("/allUsers");
-      return;
-    }
-    if (!login.equals(user.getEmail())
-            & !login.isEmpty()
-            & !pass.equals(user.getPassword())
-            & !pass.isEmpty()
-            & pass.equals(repeatPassword)) {
+    if (!login.equals(user.getEmail()) & !login.isEmpty()) {
       user.setEmail(login);
-      user.setPassword(pass);
-      accountService.updateUser(user);
-      resp.sendRedirect("/allUsers");
-      return;
     }
-    if (!login.equals(user.getEmail()) & pass.isEmpty() & repeatPassword.isEmpty()) {
-      user.setEmail(login);
-      accountService.updateUser(user);
-      resp.sendRedirect("/allUsers");
-      return;
-    }
-    if ((login.equals(user.getEmail()) | login.isEmpty())
-            & !pass.equals(user.getPassword())
+    if (!pass.equals(user.getPassword())
             & pass.equals(repeatPassword)
             & !pass.isEmpty()) {
       user.setPassword(pass);
-      accountService.updateUser(user);
-      resp.sendRedirect("/allUsers");
     }
+    if (role != null && !role.equals(user.getRole())) {
+      user.setRole(role);
+    }
+    accountService.updateUser(user);
     resp.sendRedirect("/allUsers");
   }
 }
